70f5229d08e19775f8f2ed1a85271b9dba68d8fc
nuuraan <95731245+nuuraan@users.noreply.github.com>
2021-12-17 10:12:09 +0800

    Update Board.java

--- UlarTangga/src/Board.java
+++ UlarTangga/src/Board.java
@@ -1,18 +1,49 @@
-public class Board {
--	jumlahUlar
--	jumlahTangga
--	Lantai
--	jumlahLantai
--	ularTurun
--	tanggaNaik
--	lantaiPutih
--	garisLantai
--	soalUlar
--	soalTangga
--	tileFinish
--	warnaPlayer ()
--	jumlahPlayer
--	playerStep
--	koordPlayer
+public class Board extends Dice {
+    public int jumlahUlar=3;
+    public int jumlahPlayer=2;
+    public int jumlahTangga=3;
+    public int jumlahLantai=25;
+    public static void main(String[] args) {
+        for (int row = 0; row < 5; row++)
+        {
+            System.out.println("");
+            System.out.println("____________________________________");
 
+            for (int column = 0; column < 6; column++)
+            {
+                System.out.print("| " + "     ");
+            }
+        }
+        System.out.println("");
+        System.out.println("____________________________________");
+    }
+    public void Lantai(){
+
+    }
+    public boolean ularTurun(boolean salah) {return true;}
+    public boolean tanggaNaik(boolean benar) {return true;}
+    public void lantaiPutih(){
+
+    }
+    public void garisLantai(){
+
+    }
+    public void soalUlar(){
+
+    }
+    public void soalTangga(){
+
+    }
+    public void tileFinish(){
+
+    }
+    public void warnaPlayer (){
+
+    }
+    public void playerStep(){
+
+    }
+    public void koordPlayer(){
+
+    }
 }
