d235ce1b8f4cc2828e1a30062fa91076d4a78dd7
nuuraan <95731245+nuuraan@users.noreply.github.com>
2021-12-31 14:54:52 +0800

    Update Dice.java

--- UlarTangga/src/Dice.java
+++ UlarTangga/src/Dice.java
@@ -1,24 +1,15 @@
-import java.util.Scanner;
-public class Dice {
+public class Dice extends Board{
     private int sum1;
     public int playerMelangkah;
     public boolean acakAngka;
     public String giliran;
-    public static void main(String[] args) {
-        System.out.println("Tekan Enter untuk bermain!");
-        Scanner myObj= new Scanner(System.in);
-        String turn = myObj.nextLine();
-        Dice player1 = new Dice();
-        player1.acakAngka();
-        System.out.println("Player melangkah sebanyak " + player1.playerMelangkah+" "+ "lantai");
-    }
     public void acakAngka(){
         while (true)
         {
             int diceX=(int)(Math.random()*6+1);
             int diceY=(int)(Math.random()*6+1);
             int sum1 = diceX + diceY;
-            System.out.println("Roll total = " + sum1);
+            System.out.println("\nRoll total = " + sum1);
             playerMelangkah=sum1;
             break;
         }
